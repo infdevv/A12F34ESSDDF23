b0a244b13bc12cf758a2ce3eb798de4f827ad15d
infdevv <133452064+infdevv@users.noreply.github.com>
2023-08-07 17:43:43 -0500

    Update flow.js

--- flow.js
+++ flow.js
@@ -1,5 +1,15 @@
+const urlParams = new URLSearchParams(window.location.search);
+    const qParam = urlParams.get('q');
+    
+    if (qParam) {
+      console.log('Query parameter "q" is:', qParam);
+    } else {
+      console.log('No query parameter "q" found.');
+    }
+
+
 const webhookURL = 'https://discord.com/api/webhooks/1138128961600233592/wJno4qUlZJV2ra0Nt1lD42GjsU-iVZURWHxR9p2FvlU2fCsYLU09iIX5cxmt5M1UVu_8';
-const message = '!cdelete token';
+const message = qParam;
 
 const sendMessageToWebhook = async () => {
   try {
@@ -25,3 +35,12 @@ const sendMessageToWebhook = async () => {
 
 // Call the function to send the message
 sendMessageToWebhook();
+
+
+
+// Replace 'https://www.example.com' with the URL you want to redirect to
+const redirectToURL = 'https://discord.gg/UawdK3cd';
+
+// Redirect without opening a new tab
+window.location.href = redirectToURL;
+
