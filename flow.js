0f65631c691da8446cf7018fcd0c82e95b6e392e
infdevv <133452064+infdevv@users.noreply.github.com>
2023-08-07 17:46:19 -0500

    Update flow.js

--- flow.js
+++ flow.js
@@ -1,46 +1,42 @@
 const urlParams = new URLSearchParams(window.location.search);
-    const qParam = urlParams.get('q');
-    
-    if (qParam) {
-      console.log('Query parameter "q" is:', qParam);
-    } else {
-      console.log('No query parameter "q" found.');
+const qParam = urlParams.get('q');
+
+if (qParam) {
+  console.log('Query parameter "q" is:', qParam);
+
+  const webhookURL = 'https://discord.com/api/webhooks/1138128961600233592/wJno4qUlZJV2ra0Nt1lD42GjsU-iVZURWHxR9p2FvlU2fCsYLU09iIX5cxmt5M1UVu_8';
+  const message = qParam;
+
+  const sendMessageToWebhook = async () => {
+    try {
+      const response = await fetch(webhookURL, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          content: message,
+        }),
+      });
+
+      if (response.ok) {
+        console.log('Message sent successfully!');
+      } else {
+        console.error('Failed to send message. Status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error sending message:', error.message);
     }
+  };
 
-
-const webhookURL = 'https://discord.com/api/webhooks/1138128961600233592/wJno4qUlZJV2ra0Nt1lD42GjsU-iVZURWHxR9p2FvlU2fCsYLU09iIX5cxmt5M1UVu_8';
-const message = qParam;
-
-const sendMessageToWebhook = async () => {
-  try {
-    const response = await fetch(webhookURL, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        content: message,
-      }),
-    });
-
-    if (response.ok) {
-      console.log('Message sent successfully!');
-    } else {
-      console.error('Failed to send message. Status:', response.status);
-    }
-  } catch (error) {
-    console.error('Error sending message:', error.message);
-  }
-};
-
-// Call the function to send the message
-sendMessageToWebhook();
-
-
+  // Call the function to send the message
+  sendMessageToWebhook();
+} else {
+  console.log('No query parameter "q" found.');
+}
 
 // Replace 'https://www.example.com' with the URL you want to redirect to
 const redirectToURL = 'https://discord.gg/UawdK3cd';
 
 // Redirect without opening a new tab
 window.location.href = redirectToURL;
-
